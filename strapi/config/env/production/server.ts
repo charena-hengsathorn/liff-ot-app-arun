616baa34e96ca197a3ab290e294d335af0fbad45
cartoon9281 <peteboonmang@gmail.com>
2025-11-14 16:27:01 -0800

    Fix Strapi port configuration to use STRAPI_PORT

--- strapi/config/env/production/server.ts
+++ strapi/config/env/production/server.ts
@@ -1,12 +1,17 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('STRAPI_URL', 'https://liff-ot-app-arun-d0ff4972332c.herokuapp.com'),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+export default ({ env }) => {
+  // Use STRAPI_PORT if set (for same-app deployment), otherwise PORT or default to 1337
+  const port = env.int('STRAPI_PORT') || 1337;
+  
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port,
+    url: env('STRAPI_URL', 'https://liff-ot-app-arun-d0ff4972332c.herokuapp.com'),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  };
+};
 
